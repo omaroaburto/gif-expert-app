3d4be233108805211a53c8177c79ba2362dea61d
Omaro Alexis Aburto Leiva <oaburto.freelance@gmail.com>
2021-03-13 16:30:13 -0300

    aplicación final+ docs

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,16 +1,18 @@
- const getGif = async (category) =>{
-    const API_KEY ='9nh9gUaGYIGL1KI6ogB963BZJ1iWR6gS'; // agrega tu apikey de giphy
-    const url = `https://api.giphy.com/v1/gifs/search?limit=12&q=${encodeURI(category)}&api_key=${API_KEY}`;
-    const respuesta = await fetch(url);
-    const {data} = await respuesta.json();
-    const gif = data.map( img => {
-        return{
-            id: img.id,
-            title: img.title,
-            url: img.images?.downsized_medium.url
-        }
-    }); 
-    return gif;
-} 
+ const getGif = async (category) => {
+     const API_KEY = '9nh9gUaGYIGL1KI6ogB963BZJ1iWR6gS'; // agrega tu apikey de giphy
+     const url = `https://api.giphy.com/v1/gifs/search?limit=12&q=${encodeURI(category)}&api_key=${API_KEY}`;
+     const respuesta = await fetch(url);
+     const {
+         data
+     } = await respuesta.json();
+     const gif = data.map(img => {
+         return {
+             id: img.id,
+             title: img.title,
+             url: img.images?.downsized_medium.url
+         }
+     });
+     return gif;
+ }
 
-export default getGif
+ export default getGif
