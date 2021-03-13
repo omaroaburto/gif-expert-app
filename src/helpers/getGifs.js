06164ebddaf0239d87f90c5427f01826afd136e5
Omaro Aburto Leiva <70820516+omaroaburto@users.noreply.github.com>
2021-03-13 16:02:51 -0300

    Update getGifs.js
    api key comentario

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,6 @@
  const getGif = async (category) =>{
-    const url = `https://api.giphy.com/v1/gifs/search?limit=12&q=${encodeURI(category)}&api_key=9nh9gUaGYIGL1KI6ogB963BZJ1iWR6gS`;
+    const API_KEY =''; // agrega tu apikey de giphy
+    const url = `https://api.giphy.com/v1/gifs/search?limit=12&q=${encodeURI(category)}&api_key=${API_KEY}`;
     const respuesta = await fetch(url);
     const {data} = await respuesta.json();
     const gif = data.map( img => {
