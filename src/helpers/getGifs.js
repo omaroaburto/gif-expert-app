53084aa8a22319c16007641b14e4489be2385857
Omaro Alexis Aburto Leiva <oaburto.freelance@gmail.com>
2021-03-13 16:20:05 -0300

    aplicación final+ docs

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
  const getGif = async (category) =>{
-    const API_KEY =''; // agrega tu apikey de giphy
+    const API_KEY ='9nh9gUaGYIGL1KI6ogB963BZJ1iWR6gS'; // agrega tu apikey de giphy
     const url = `https://api.giphy.com/v1/gifs/search?limit=12&q=${encodeURI(category)}&api_key=${API_KEY}`;
     const respuesta = await fetch(url);
     const {data} = await respuesta.json();
